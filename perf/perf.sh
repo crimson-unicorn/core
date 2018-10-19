fc795b5a92aabf5926782077e8519057bfda66c8
Michael Xueyuan Han <hanx@g.harvard.edu>
2018-10-19 11:28:32 +0100

    [ci skip] update perf.sh

--- perf/perf.sh
+++ perf/perf.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 while :
 do 
-	top -bn1 -p $(pidof camflow-provenance) | grep "^ " | awk '{ printf("%-8s %-8s\n", $9, $10); }'
-	ps --no-header -o "%cpu %mem" -p $(pidof camflow-provenance)
-	#ps -p $(pidof camflow-provenance) -L -o psr,pcpu
-	# echo $(pidof camflow-provenance)
+	top -bn1 -p $(pidof main) | grep "^ " | awk '{ printf("%-8s %-8s\n", $9, $10); }'
+	ps --no-header -o "%cpu %mem" -p $(pidof main)
+	#ps -p $(pidof main) -L -o psr,pcpu
+	# echo $(pidof main)
 	sleep 1
 done
