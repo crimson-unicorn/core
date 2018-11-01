8fffa26a148ca6f6b39fcb91cafd5acd28d3d415
Michael Xueyuan Han <hanx@g.harvard.edu>
2018-11-01 14:49:04 +0000

    performance script

--- perf/perf.sh
+++ perf/perf.sh
@@ -3,7 +3,7 @@ while :
 do 
 	top -bn1 -p $(pidof main) | grep "^ " | awk '{ printf("%-8s %-8s\n", $9, $10); }'
 	ps --no-header -o "%cpu %mem" -p $(pidof main)
-	#ps -p $(pidof main) -L -o psr,pcpu
+	ps -p $(pidof main) -L -o psr,pcpu
 	# echo $(pidof main)
 	sleep 1
 done
